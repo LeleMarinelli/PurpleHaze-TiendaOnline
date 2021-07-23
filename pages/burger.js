08ba798f87203ef4fb16408b640546d8cb3b434b
Lele Marinelli <leancymarinelli@gmail.com>
2021-07-23 20:26:23 -0300

    accesibilidad del menu

--- pages/burger.js
+++ pages/burger.js
@@ -2,7 +2,13 @@ const menu = document.querySelector(".menu");
 const toggle = document.querySelector(".toggle");
 
 toggle.addEventListener("click", () => {
-    menu.classList.toggle("menu_visible")
-})
+    menu.classList.toggle("menu_visible");
+
+    if (menu.classList.contains("menu_visible")) {
+        toggle.setAttribute("aria-label", "Cerrar menu");
+    } else {
+        toggle.setAttribute("aria-label", "Abrir menu");
+    }
+});
 
 
