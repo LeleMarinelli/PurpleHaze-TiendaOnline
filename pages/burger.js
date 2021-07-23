dcd1fbe9e4d69cd0fc73d4c73052ac39c9433128
Lele Marinelli <leancymarinelli@gmail.com>
2021-07-23 19:17:42 -0300

    se agrego el boton de hamburguesa y se agrego un poco de codigo javascript

---
+++ pages/burger.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("menu_visible")
+})
+
+
