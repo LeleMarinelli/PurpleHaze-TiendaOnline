c5c904448a36b3a514577dc8c57560be04bf85e1
Lele Marinelli <leancymarinelli@gmail.com>
2021-07-24 12:17:37 -0300

    funcionamiento de la pag colecciones

--- pages/burger.js
+++ pages/burger.js
@@ -1,5 +1,5 @@
-const menu = document.querySelector(".menu");
 const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
 
 toggle.addEventListener("click", () => {
     menu.classList.toggle("menu_visible");
